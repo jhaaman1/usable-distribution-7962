9e0816a45a71d3620f3bd35822eb228155d11376
jhaaman1 <ajha9608@gmail.com>
2022-08-27 08:02:22 +0530

    add day-3 work

--- src/Routes/PlayerSearch.js
+++ src/Routes/PlayerSearch.js
@@ -1,9 +1,15 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { getPlayerStatus, playerSearh } from '../api/api';
 
 function PlayerSearch() {
-  return (
-    <div>PlayerSearch</div>
-  )
-}
+  const [playerdata, setPlayerData] = useState([]);
 
+  useEffect(() => {
+    playerSearh()
+      .then((res) => res.json())
+      .then((res) => setPlayerData())
+      .catch((err) => console.log(err))
+  })
+}
+console.log((playerdata));
 export default PlayerSearch
--- src/api/api.js
+++ src/api/api.js
@@ -1,9 +1,17 @@
-const API_KEY = "49f2de12-9358-4b06-9309-42a88fe96732";
+import axios from "axios";
+
+// const API_KEY = "49f2de12-9358-4b06-9309-42a88fe96732";
+const API_KEY = "0c064f33-562b-4d36-898e-f3cac24dd2d2";
 const NEWS_API_KEY = 'dd1b525e290741b8b413c2fb66346d5b';
 
 export const  getMatchesInfo = async () => {
-    const url = `https://api.cricapi.com/v1/currentMatches?apikey=49f2de12-9358-4b06-9309-42a88fe96732&offset=0`
+    const url = `https://api.cricapi.com/v1/currentMatches?apikey=${API_KEY}&offset=0`
     // return await fetch(url);
+}
+
+export const getCricketScore =  (id) => {
+  const url = `https://api.cricapi.com/v1/cricScore?apikey=${API_KEY}&unique_id=${id}`
+  return fetch(url)
 }
 
 export const getNewsInfo = async () => {
@@ -11,18 +19,22 @@ export const getNewsInfo = async () => {
     return await fetch(url);
 }
 
+export const playerSearh = (name, id) => {
+  return await fetch(`https://api.cricapi.com/v1/players_info?apikey=${API_KEY}&name=${name}_id=${id}`)
+}
 
-const axios = require("axios");
 
- const options = {
-  method: 'GET',
-  url: 'https://cricbuzz-cricket.p.rapidapi.com/news/v1/topics/349',
-  headers: {
-    'X-RapidAPI-Key': '369e8a1e45msh8dcba889d30c4a7p1cd380jsnbdebbfbcce37',
-    'X-RapidAPI-Host': 'cricbuzz-cricket.p.rapidapi.com'
-  }
-};
+// const options = {
+//   method: 'GET',
+//   url: 'https://cricbuzz-cricket.p.rapidapi.com/matches/v1/recent',
+//   headers: {
+//     'X-RapidAPI-Key': '369e8a1e45msh8dcba889d30c4a7p1cd380jsnbdebbfbcce37',
+//     'X-RapidAPI-Host': 'cricbuzz-cricket.p.rapidapi.com'
+//   }
+// };
 
-export const getNewsData =() =>{
-    return axios.request(options)
-} 
+// axios.request(options).then(function (response) {
+// 	console.log(response.data);
+// }).catch(function (error) {
+// 	console.error(error);
+// });
